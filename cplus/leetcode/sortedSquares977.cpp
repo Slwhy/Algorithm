605b0685cee9c2ea3f4c72fb4e2ffb64638ce43d
Slwhy <3209029522@qq.com>
2021-08-09 20:34:15 +0800

    20210809

---
+++ cplus/leetcode/sortedSquares977.cpp
@@ -0,0 +1,40 @@
+//
+// Created by slwhy on 2021/8/9.
+// Desc:977. 有序数组的平方
+//
+#include "vector"
+
+using namespace std;
+
+vector<int> sortedSquares1(vector<int> &nums) {
+    /*
+     * 双指针法
+     */
+    vector<int> res(nums.size());
+    int tmp = 0, left = 0, right = nums.size() - 1;
+    for (int i = 0; i < nums.size(); ++i) {
+        res[i] = nums[i] * nums[i];
+    }
+    while (left < right) {
+        if (nums[left] > right) {
+            tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+        }
+        right--;
+
+    }
+    return res;
+}
+
+vector<int> sortedSquares(vector<int> &nums) {
+    /*
+     * 先平方,然后排序
+     */
+    vector<int> res(nums.size());
+    for (int i = 0; i < nums.size(); ++i) {
+        res[i] = nums[i] * nums[i];
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
