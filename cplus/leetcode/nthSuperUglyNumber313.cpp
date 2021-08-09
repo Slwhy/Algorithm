23e10dd00db8ef1f0c54bcd0d333d585b4b9e1a7
slwhy <3209029522@qq.com>
2021-08-10 00:13:50 +0800

    20210809

---
+++ cplus/leetcode/nthSuperUglyNumber313.cpp
@@ -0,0 +1,27 @@
+//
+// Created by slwhy on 2021/8/9.
+// Desc:313. 超级丑数
+//
+#include "vector"
+
+using namespace std;
+
+int nthSuperUglyNumber(int n, vector<int> &primes) {
+    unordered_set<long> seen;
+    priority_queue<long, vector<long>, greater<long>> heap;
+    seen.insert(1);
+    heap.push(1);
+    int ugly = 0;
+    for (int i = 0; i < n; i++) {
+        long curr = heap.top();
+        heap.pop();
+        ugly = (int)curr;
+        for (int prime : primes) {
+            long next = curr * prime;
+            if (seen.insert(next).second) {
+                heap.push(next);
+            }
+        }
+    }
+    return ugly;
+}
