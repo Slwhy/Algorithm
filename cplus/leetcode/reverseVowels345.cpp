eb7a14e95e2a0fc77109ea0e19a2462f0b693e25
Slwhy <3209029522@qq.com>
2021-08-23 20:47:03 +0800

    20210823

--- cplus/leetcode/reverseVowels345.cpp
+++ cplus/leetcode/reverseVowels345.cpp
@@ -1,5 +1,29 @@
 //
 // Created by slwhy on 2021/8/19.
-// Desc:
+// Desc:345. 反转字符串中的元音字母
 //
+
+#include "vector"
+#include "string"
+#include "set"
+#include "iostream"
+
+using namespace std;
+//vector<char> vowelVec({'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'});
+set<char> vowel = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+string reverseVowels(string s) {
+    int left = 0, right = s.size() - 1;
+    char tmp;
+    while (left < right) {
+        while (left < right && vowel.find(s[left]) == vowel.end()) left++;
+        while (left < right && vowel.find(s[right]) == vowel.end()) right--;
+        tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
+    }
+    return s;
+}
 
