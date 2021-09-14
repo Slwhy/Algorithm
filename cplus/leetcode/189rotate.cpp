3d38d0f4dbcda19400ecc22335e0504f1d1ac07a
Slwhy <3209029522@qq.com>
2021-09-14 21:00:32 +0800

    20210914

--- cplus/leetcode/189rotate.cpp
+++ cplus/leetcode/189rotate.cpp
@@ -1,5 +1,34 @@
 //
 // Created by slwhy on 2021/9/14.
-// Desc:
+// Desc:189. 旋转数组
 //
+#include "vector"
+#include "queue"
 
+using namespace std;
+
+void rotate2(vector<int> &nums, int k) {
+    vector<int> rightVec(k);
+    int len = nums.size();
+    for (int i = 0; i < k; ++i) {
+        rightVec[k-i-1] = nums[len-i-1];
+    }
+    for (int i = len-k-1; i >=0 ; --i) {
+        nums[(i+k)%len] = nums[i];
+    }
+    for (int i = 0; i < k; ++i) {
+        nums[i] = rightVec[i];
+    }
+}
+
+void rotate(vector<int> &nums, int k) {
+    /*
+     * 暴力，用数组存储所有整个数组
+     */
+    int len = nums.size();
+    vector<int> res(len);
+    for (int i = 0; i < len; ++i) {
+        res[(i + k) % len] = nums[i];
+    }
+    nums = res;
+}
